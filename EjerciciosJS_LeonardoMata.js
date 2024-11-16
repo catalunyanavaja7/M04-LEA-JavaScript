40c350bbb14b54ba8be9c94a4a750773731a66ff
yolo1 <leonardo.mata@institutvidreres.cat>
2024-11-16 16:26:10 +0100

    Ejercicios del 1 al 5

---
+++ EjerciciosJS_LeonardoMata.js
@@ -0,0 +1,54 @@
+// Ejercicio 1
+console.log("=====Ejercicio 1=====")
+
+
+console.log("Leo")
+console.log("Mata")
+console.log("01/01/2005")
+
+
+// Ejercicio 2
+console.log("=====Ejercicio 2=====")
+
+
+const var1 = 5.3;
+var var2 = "Texto";
+let var3 = 3;
+
+
+console.log(var1);
+console.log(var2);
+console.log(var3);
+
+
+//Ejercicio 3
+console.log("=====Ejercicio 3=====")
+
+
+console.log(variableprimero)
+var variableprimero = 6
+// Con "let" da error, lo enseño en la practica.
+
+
+// Ejercicio 4
+console.log("=====Ejercicio 4=====")
+
+
+var numero = 2;
+console.log(numero);
+
+
+numero = "Hola";
+console.log(numero)
+
+
+// Ejercicio 5
+console.log("=====Ejercicio 5=====")
+
+
+const VALOR = "Hola";
+console.log(VALOR)
+/* Si le cambio el valor a la constant da el error de la captura de la practica.
+VALOR = "Adios";
+console.log(VALOR);
+*/
