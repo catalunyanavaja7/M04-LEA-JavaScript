68fc45eda19948717db944c8fce7032847c6a7cc
yolo1 <leonardo.mata@institutvidreres.cat>
2024-11-18 08:31:45 +0100

    Practica 2 Ejercicio 1

--- Practica_2/JSFuncions_Leonardo_Mata.js
+++ Practica_2/JSFuncions_Leonardo_Mata.js
@@ -1,1 +1,9 @@
+// Ejercicio 1
+console.log("=====Ejercicio 1=====")
+
+function nombreapellido (nombre, apellido) {
+    console.log("Mi nombre es " + nombre + apellido)
+}
+
+nombreapellido("Leo ","Mata")
 
