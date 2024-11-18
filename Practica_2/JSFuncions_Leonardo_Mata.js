493671a5fbd9ca5bb012ca87262f4f2103db7d97
yolo1 <leonardo.mata@institutvidreres.cat>
2024-11-18 08:47:01 +0100

    Practica 2 Ejercicio 3

--- Practica_2/JSFuncions_Leonardo_Mata.js
+++ Practica_2/JSFuncions_Leonardo_Mata.js
@@ -14,5 +14,12 @@ function dequetipo (numero) {
 }
 dequetipo(6)
 
+// Ejercicio 3
+console.log("=====Ejercicio 3=====")
 
+const hoy = new Date();
 
+function formatoFecha(fecha, formato) {
+    console.log(fecha + formato)
+}
+formatoFecha(hoy, ' 18/11/24');
