6ac04ba61c6a80df772e03184fec4202607c1aa2
yolo1 <leonardo.mata@institutvidreres.cat>
2024-11-18 08:37:01 +0100

    Practica 2 Ejercicio 2

--- Practica_2/JSFuncions_Leonardo_Mata.js
+++ Practica_2/JSFuncions_Leonardo_Mata.js
@@ -4,6 +4,15 @@ console.log("=====Ejercicio 1=====")
 function nombreapellido (nombre, apellido) {
     console.log("Mi nombre es " + nombre + apellido)
 }
-
 nombreapellido("Leo ","Mata")
+
+// Ejercicio 2
+console.log("=====Ejercicio 2=====")
+
+function dequetipo (numero) {
+    console.log("Este valor es de tipo: " + typeof numero)
+}
+dequetipo(6)
+
+
 
